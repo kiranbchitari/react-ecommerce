474f3d52e762eec0f1e3ca1daf4c196558c7b75b
Kiran B Chitari <kiranbchitari@gmail.com>
2023-08-14 22:41:23 +0530

    Routing added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import ProductList from './features/productList/ProductList';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage/>,
+  },
+]);
+
 
 function App() {
   return (
     <div className='App'>
-      <Home/>
+     <RouterProvider router={router}/>
     </div>
   )
 }
